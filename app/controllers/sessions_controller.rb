453681d85bc6767b15f02aea72590475559386ae
Anais <anais.cuisinier@gmail.com>
2017-08-01 11:26:06 +0200

    updating user1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,13 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
-  	if
-  		user && user.authenticate(params[:session][:password])
+  	if user && user.authenticate(params[:session][:password])
   		log_in user
-  		if params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  			redirect_to user
-  		else 
+  		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  		redirect_back_or user
+  	else 
   			flash.now[:danger] = "Invalid password combination"
   			render 'new'
-  		end
   	end
   end
 

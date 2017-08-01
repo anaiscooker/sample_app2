c6231f60e949a6035846d44f58cb2862b70cf239
Anais <anais.cuisinier@gmail.com>
2017-08-01 09:23:34 +0200

    week_5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,17 @@ class SessionsController < ApplicationController
   	if
   		user && user.authenticate(params[:session][:password])
   		log_in user
-  		redirect_to user
-  	else 
-  		flash.now[:danger] = "Invalid password combination"
-  		render 'new'
+  		if params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  			redirect_to user
+  		else 
+  			flash.now[:danger] = "Invalid password combination"
+  			render 'new'
+  		end
   	end
   end
 
   def destroy
-  	log_out
+  	log_out if logged_in?
   	redirect_to root_url
   end
 
